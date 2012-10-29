64511a66803f8a1d1a45c0018c5463b8a9461329
Naoto Mawatari <github.mawatari@gmail.com>
2012-10-29 11:48:47 +0900

    4.4 アクションの作成、ビューテンプレートの表示 : index, doneアクションの作成と、indexビューの作成

--- app/Controller/TasksController.php
+++ app/Controller/TasksController.php
@@ -2,4 +2,17 @@
 
 class TasksController extends AppController {
 	public $scaffold;
+	public $helper = array('Html', 'Form');
+
+	public function index() {
+		$tasks_data = array();
+		$this->set('tasks_data', $tasks_data);
+
+		$this->render('index');
+	}
+
+	public function done() {
+		$msg = sprintf('タスク %s を完了しました。', $this->request->pass[0]);
+		$this->flash($msg, '/Tasks/index');
+	}
 }
